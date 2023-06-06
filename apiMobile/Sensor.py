bd4d99b746e77585807b0deb868019af382229f0
BrenoFragaDosAnjos <breno.anjos.bf@hotmail.com>
2023-06-06 20:54:22 -0300

    mudancas api

--- apiMobile/Sensor.py
+++ apiMobile/Sensor.py
@@ -1,14 +1,28 @@
 class Sensor:
-    def __init__(self,nome,valor,data_captura):
-        self.nome = nome
-        self.valor = valor
+    def __init__(self,id_viagem,temperatura,umidade,vibracao,ruido,proximidade,peso,latitude,longitude,data_captura):
+        self.id_viagem = id_viagem
+        self.temperatura = temperatura
+        self.umidade = umidade
+        self.vibracao = vibracao
+        self.ruido = ruido
+        self.proximidade = proximidade
+        self.peso = peso
+        self.latitude = latitude
+        self.longitude = longitude
         self.data_captura = data_captura
         pass
 
     def to_dict(self):
         return {
-            'nome': self.nome,
-            'valor': self.valor,
+            'id_viagem': self.id_viagem,
+            'temperatura': self.temperatura,
+            'umidade': self.umidade,
+            'vibracao' : self.vibracao,
+            'ruido' : self.ruido,
+            'proximidade' : self.proximidade,
+            'peso' : self.peso,
+            'latitude' : self.latitude,
+            'longitude' : self.longitude,
             'data_captura': self.data_captura.strftime('%Y-%m-%d %H:%M:%S')
         }
 
--- apiMobile/connectDB.py
+++ apiMobile/connectDB.py
@@ -1,193 +1,12 @@
 import pyodbc
 import Sensor as sen
 
-def recuperarUmidade():
-    sensores = []
-    conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:wordserver.database.windows.net,1433;Database=word-database;Uid=urubu100;Pwd=14052002Kb4_;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    cursor = conn.cursor()
-    sql = "SELECT * FROM SensorData"
-    cursor.execute(sql)
-    rows = cursor.fetchall()  # Chamada corrigida
-
-    if len(rows) == 0:
-        return {"success": True, "data": ""}
-
-    else:
-        for row in rows:
-        # Extrair os valores das colunas
-            nome = row[1]
-            valor = row[2]
-            data_captura = row[3]
-
-            sensor = sen.Sensor(nome, valor, data_captura)
-            sensores.append(sensor.to_dict())
-
-    response = {"success": True, "data": sensores}
-    print(sensores)
-    conn.close()
-    return response
-
-def recuperarTemperatura():
-    sensores = []
-    conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:wordserver.database.windows.net,1433;Database=word-database;Uid=urubu100;Pwd=14052002Kb4_;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    cursor = conn.cursor()
-    sql = "SELECT *FROM [dbo].[SensorData] WHERE SensorName = 'temperatura'"
-    cursor.execute(sql)
-    rows = cursor.fetchall()  # Chamada corrigida
-
-    if len(rows) == 0:
-        return {"success": True, "data": ""}
-
-    else:
-        for row in rows:
-        # Extrair os valores das colunas
-            nome = row[1]
-            valor = row[2]
-            data_captura = row[3]
-
-            sensor = sen.Sensor(nome, valor, data_captura)
-            sensores.append(sensor.to_dict())
-
-    response = {"success": True, "data": sensores}
-    print(sensores)
-    conn.close()
-    return response
-
-def recuperarVibracao():
-    sensores = []
-    conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:wordserver.database.windows.net,1433;Database=word-database;Uid=urubu100;Pwd=14052002Kb4_;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    cursor = conn.cursor()
-    sql = "SELECT *FROM [dbo].[SensorData] WHERE SensorName = 'vibracao'"
-    cursor.execute(sql)
-    rows = cursor.fetchall()  # Chamada corrigida
-
-    if len(rows) == 0:
-        return {"success": True, "data": ""}
-
-    else:
-        for row in rows:
-        # Extrair os valores das colunas
-            nome = row[1]
-            valor = row[2]
-            data_captura = row[3]
-
-            sensor = sen.Sensor(nome, valor, data_captura)
-            sensores.append(sensor.to_dict())
-
-    response = {"success": True, "data": sensores}
-    print(sensores)
-    conn.close()
-    return response
-
-def recuperarRuido():
-    sensores = []
-    conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:wordserver.database.windows.net,1433;Database=word-database;Uid=urubu100;Pwd=14052002Kb4_;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    cursor = conn.cursor()
-    sql = "SELECT *FROM [dbo].[SensorData] WHERE SensorName = 'ruido'"
-    cursor.execute(sql)
-    rows = cursor.fetchall()  # Chamada corrigida
-
-    if len(rows) == 0:
-        return {"success": True, "data": ""}
-
-    else:
-        for row in rows:
-        # Extrair os valores das colunas
-            nome = row[1]
-            valor = row[2]
-            data_captura = row[3]
-
-            sensor = sen.Sensor(nome, valor, data_captura)
-            sensores.append(sensor.to_dict())
-
-    response = {"success": True, "data": sensores}
-    print(sensores)
-    conn.close()
-    return response
-
-def recuperarProximidade():
-    sensores = []
-    conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:wordserver.database.windows.net,1433;Database=word-database;Uid=urubu100;Pwd=14052002Kb4_;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    cursor = conn.cursor()
-    sql = "SELECT *FROM [dbo].[SensorData] WHERE SensorName = 'proximidade'"
-    cursor.execute(sql)
-    rows = cursor.fetchall()  # Chamada corrigida
-
-    if len(rows) == 0:
-        return {"success": True, "data": ""}
-
-    else:
-        for row in rows:
-        # Extrair os valores das colunas
-            nome = row[1]
-            valor = row[2]
-            data_captura = row[3]
-
-            sensor = sen.Sensor(nome, valor, data_captura)
-            sensores.append(sensor.to_dict())
-
-    response = {"success": True, "data": sensores}
-    print(sensores)
-    conn.close()
-    return response
-
-def recuperarPeso():
-    sensores = []
-    conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:wordserver.database.windows.net,1433;Database=word-database;Uid=urubu100;Pwd=14052002Kb4_;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    cursor = conn.cursor()
-    sql = "SELECT *FROM [dbo].[SensorData] WHERE SensorName = 'peso'"
-    cursor.execute(sql)
-    rows = cursor.fetchall()  # Chamada corrigida
-
-    if len(rows) == 0:
-        return {"success": True, "data": ""}
-
-    else:
-        for row in rows:
-        # Extrair os valores das colunas
-            nome = row[1]
-            valor = row[2]
-            data_captura = row[3]
-
-            sensor = sen.Sensor(nome, valor, data_captura)
-            sensores.append(sensor.to_dict())
-
-    response = {"success": True, "data": sensores}
-    print(sensores)
-    conn.close()
-    return response
-
-def recuperarTemperatura():
-    sensores = []
-    conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:wordserver.database.windows.net,1433;Database=word-database;Uid=urubu100;Pwd=14052002Kb4_;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    cursor = conn.cursor()
-    sql = "SELECT *FROM [dbo].[SensorData] WHERE SensorName = 'temperatura'"
-    cursor.execute(sql)
-    rows = cursor.fetchall()  # Chamada corrigida
-
-    if len(rows) == 0:
-        return {"success": True, "data": ""}
-
-    else:
-        for row in rows:
-        # Extrair os valores das colunas
-            nome = row[1]
-            valor = row[2]
-            data_captura = row[3]
-
-            sensor = sen.Sensor(nome, valor, data_captura)
-            sensores.append(sensor.to_dict())
-
-    response = {"success": True, "data": sensores}
-    print(sensores)
-    conn.close()
-    return response
 
 def recuperarAll():
     sensores = []
     conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:wordserver.database.windows.net,1433;Database=word-database;Uid=urubu100;Pwd=14052002Kb4_;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
     cursor = conn.cursor()
-    sql = "SELECT *FROM [dbo].[SensorData]"
+    sql = "select id_viagem, temperatura, umidade, vibracao, ruido, proximidade, peso, latitude, longitude, data_captura from [dbo].[sensoresIntegrados]"
     cursor.execute(sql)
     rows = cursor.fetchall()  # Chamada corrigida
 
@@ -197,14 +16,24 @@ def recuperarAll():
     else:
         for row in rows:
         # Extrair os valores das colunas
-            nome = row[1]
-            valor = row[2]
-            data_captura = row[3]
+            id_viagem = row[0]
+            temperatura = row[1]
+            umidade = row[2]
+            vibracao = row[3]
+            ruido = row[4]
+            proximidade = row[5]
+            peso = row[6]
+            latitude = row[7]
+            longitude = row[8]
+            data_captura = row[9]
+           
+            
 
-            sensor = sen.Sensor(nome, valor, data_captura)
+            sensor = sen.Sensor(id_viagem,temperatura,umidade,vibracao,ruido,proximidade,peso,latitude,longitude,data_captura)
             sensores.append(sensor.to_dict())
 
     response = {"success": True, "data": sensores}
     print(sensores)
     conn.close()
     return response
+
